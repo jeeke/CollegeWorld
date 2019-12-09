b16037bfa5059a6ad289971a9c5c6afa030fef9e
Gyanesh <gyanesh.formal@gmail.com>
2019-12-09 18:59:00 +0530

    first commit

--- app/src/main/java/com/collegeworld/ui/auth/MainActivity.kt
+++ app/src/main/java/com/collegeworld/ui/auth/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.collegeworld
+package com.collegeworld.ui.auth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.collegeworld.R
 
 class MainActivity : AppCompatActivity() {
 
